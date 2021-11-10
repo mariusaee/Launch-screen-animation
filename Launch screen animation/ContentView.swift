226701bcd4fa7861c832f341bb94bb43845b99d6
mariusaee <mariusae@me.com>
2021-11-10 15:37:50 +0300

    Initial Commit

---
+++ Launch screen animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Launch screen animation
+//
+//  Created by Marius Malyshev on 10.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Launch screen animation/Launch_screen_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launch_screen_animationApp.swift
+//  Launch screen animation
+//
+//  Created by Marius Malyshev on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Launch_screen_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
